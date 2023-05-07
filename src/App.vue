88e7ee1c4d8c522f5440fe9e515d1e20f49ed13b
=brayanpedroso <=brayanmattos9@gmail.com>
2023-05-07 12:33:32 -0300

    atualizaçao

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+  import { ref } from 'vue'
 
 
 const produtos = [
@@ -53,29 +54,113 @@ const produtos = [
         preco: 9.90
     }
 ]
-  
+const carrinho = ref([
+
+    {
+        id: 1,
+        nome: 'Camiseta',
+        preco: 49.90,
+        quantidade: 1,
+        valorTotal: 49.90
+    },
+    {
+        id: 2,
+        nome: 'Calça',
+        preco: 99.90,
+        quantidade: 1,
+        valorTotal: 49.90
+    },
+    {
+        id: 3,
+        nome: 'Meia',
+        preco: 9.90,
+        quantidade: 1,
+        valorTotal: 49.90
+    },
+    {
+        id: 4,
+        nome: 'Sapato',
+        preco: 199.90,
+        quantidade: 1,
+        valorTotal: 49.90
+    },
+    {
+        id: 5,
+        nome: 'Boné',
+        preco: 29.90,
+        quantidade: 1,
+        valorTotal: 49.90
+    },
+    {
+        id: 6,
+        nome: 'Óculos',
+        preco: 99.90,
+        quantidade: 1,
+        valorTotal: 49.90
+    },
+    {
+        id: 7,
+        nome: 'Relógio',
+        preco: 299.90,
+        quantidade: 1,
+        valorTotal: 49.90
+    },
+    {
+        id: 8,
+        nome: 'Bermuda',
+        preco: 79.90,
+        quantidade: 1,
+        valorTotal: 49.90
+    },
+    {
+        id: 9,
+        nome: 'Cueca',
+        preco: 19.90,
+        quantidade: 1,
+        valorTotal: 49.90
+    },
+    {
+        id: 10,
+        nome: 'Meia',
+        preco: 9.90,
+        quantidade: 1,
+        valorTotal: 49.90
+    }
+])
+const botao = ref(false)
+function bot (){
+    botao.value = true
+}
 
 
 </script>
 
 <template>
-<div class="container-fluid">
-  <h1>loja CRF</h1>
-   <img src="flamengo.webp" alt="fla">
-</div>
-<div>
-  <ul>
-<li v-for="items in produtos" :key="items.id" >{{ items.nome   }} - {{ items.preco }}
-<button class="bot">adicionar ao carrinho</button>
+    <div class="container-fluid">
+        <h1>loja CRF</h1>
+        <img src="flamengo.webp" alt="fla">
+    </div>
+    <div>
+        <ul>
+            <li v-for="items in produtos" :key="items.id">{{ items.nome }} - {{ items.preco }}
+                <button @click="adicionar" class="bot">adicionar ao carrinho</button>
+            </li>
+        </ul>
+    </div>
+    <button @click="bot" type="button" class="btn btn-primary">Mostrar carrinho</button>
 
-
- </li>
-
-</ul></div>
-   
-
+    <div v-if="botao == true">
+        <ul>
+            <li v-for="car in carrinho" :key="car.id">{{ car.nome }} - {{ car.preco }} - {{ car.quantidade }}
+                <button class="bot">retirar do carrinho</button>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <style scoped>
-
+.container-fluid {
+    margin: 0 auto;
+    text-align: center;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap'
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap'
 createApp(App).mount('#app')
