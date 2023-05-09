fa3a8787af8601aa2f5ac542db1ef75048210482
= <=>
2023-05-09 11:50:49 -0300

    atualizaçao

--- src/App.vue
+++ src/App.vue
@@ -54,48 +54,47 @@ const produtos = [
         preco: 9.90
     }
 ]
-const carrinho = ref([
-
+const carrinho = ref( [
     {
         id: 1,
         nome: 'Camiseta',
         preco: 49.90,
-        quantidade: 1,
+        quantidade: 0,
         valorTotal: 49.90
     },
     {
         id: 2,
         nome: 'Calça',
         preco: 99.90,
-        quantidade: 1,
+        quantidade: 0,
         valorTotal: 49.90
     },
     {
         id: 3,
         nome: 'Meia',
         preco: 9.90,
-        quantidade: 1,
+        quantidade: 0,
         valorTotal: 49.90
     },
     {
         id: 4,
         nome: 'Sapato',
         preco: 199.90,
-        quantidade: 1,
+        quantidade: 0,
         valorTotal: 49.90
     },
     {
         id: 5,
         nome: 'Boné',
         preco: 29.90,
-        quantidade: 1,
+        quantidade: 0,
         valorTotal: 49.90
     },
     {
         id: 6,
         nome: 'Óculos',
         preco: 99.90,
-        quantidade: 1,
+        quantidade: 0,
         valorTotal: 49.90
     },
     {
@@ -109,28 +108,41 @@ const carrinho = ref([
         id: 8,
         nome: 'Bermuda',
         preco: 79.90,
-        quantidade: 1,
+        quantidade: 0,
         valorTotal: 49.90
     },
     {
         id: 9,
         nome: 'Cueca',
         preco: 19.90,
-        quantidade: 1,
+        quantidade: 0,
         valorTotal: 49.90
     },
     {
         id: 10,
         nome: 'Meia',
         preco: 9.90,
-        quantidade: 1,
+        quantidade: 0,
         valorTotal: 49.90
     }
 ])
+
 const botao = ref(false)
 function bot (){
     botao.value = true
 }
+function adicionar(produto){
+    const index = carrinho.value.findIndex(item => item.id === produto.id)
+    if (index > -1) {
+        carrinho.value[index].quantidade++
+    }     
+}
+function remover(produto){
+ 
+    }
+
+
+
 
 
 </script>
@@ -143,7 +155,7 @@ function bot (){
     <div>
         <ul>
             <li v-for="items in produtos" :key="items.id">{{ items.nome }} - {{ items.preco }}
-                <button @click="adicionar" class="bot">adicionar ao carrinho</button>
+                <button @click="adicionar(items)" class="bot">adicionar ao carrinho</button>
             </li>
         </ul>
     </div>
@@ -152,7 +164,7 @@ function bot (){
     <div v-if="botao == true">
         <ul>
             <li v-for="car in carrinho" :key="car.id">{{ car.nome }} - {{ car.preco }} - {{ car.quantidade }}
-                <button class="bot">retirar do carrinho</button>
+                <button @click="remover" class="bot">retirar do carrinho</button>
             </li>
         </ul>
     </div>
